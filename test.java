ae11cac0eb00d9b4689928168e46d324b44dd5df
dQwaz <56117011+dQwaz@users.noreply.github.com>
2020-12-20 02:55:59 +0100

    Add files via upload

--- test.java
+++ test.java
@@ -5,14 +5,10 @@ public class test{
     public String lName;
     public String fullName;
     private static String bugFullName;
-     
+
     public static void main(String[] args){
         //scanning the strings and storing them in variables
 
-        
-
-        
-        
         System.out.println("Please enter two String");
         System.out.println();
         System.out.println();
@@ -20,7 +16,7 @@ public class test{
         String firstN = firstName.nextLine();
         Scanner lastName = new Scanner(System.in);
         String lastN = lastName.nextLine();
-        String fullName = firstN + lastN; 
+        String fullName = firstN + lastN;
         System.out.println("The appended string is "+fullName);
 
         //Counting number of character in appended string
@@ -29,40 +25,40 @@ public class test{
             if(fullName.charAt(i) != ' '){
                 fCount++;
             }
-            
+
         }
-        //bug code
+        //recreating bug
         int max = (1000 / fCount);
         int modulo = (1000 % fCount);
         int modulo_string = (int)(Math.random()* modulo);
         double x = (Math.random() * max);
         int z = (int) x;
         String bugFullName = fullName.repeat(z);
-        
-        
-        //num characters of bugstring
+
+
+        //num characters of bugged string
         int count = 0;
         for(int i = 0; i< bugFullName.length(); i++){
 
             if(bugFullName.charAt(i) != ' ')
                 count++;
         }
-        
+
         //Scanning specific character to count
         System.out.println("Please write character to count");
         Scanner character = new Scanner(System.in);
         char fCharacter = character.next().charAt(0);
         String subBug = bugFullName.substring(0, modulo_string);
-        //counting specific charatres
+        //counting specific characters
         int charCount = 0;
         for (int i = 0; i < bugFullName.length(); i++){
-        
+
             if(bugFullName.charAt(i) == fCharacter)
                 charCount++;
-               
-            
+
+
         }
-        
+        //counting specific character in sub string
         int CharCountMainString = charCount;
         int charCountSub = 0;
         for (int o = 0; o<subBug.length(); o++){
@@ -73,13 +69,7 @@ public class test{
         //printing results
         System.out.println("Number of characters is  " +count);
         System.out.println(bugFullName + bugFullName.substring(0, modulo_string));
-        
-        System.out.println("Character "+fCharacter + "was found in this string for "+finalCount+" times");
+
+        System.out.println("Character "+fCharacter + " was found in this string for "+finalCount+" times");
     }
-
-
-
-
-
-
 }
