04fb4418908017ca71f0e60fc9eb2031063ec469
dQwaz <56117011+dQwaz@users.noreply.github.com>
2020-12-20 13:53:36 +0100

    Add files via upload
    Fixed

--- test.java
+++ test.java
@@ -5,10 +5,10 @@ public class test{
     public String lName;
     public String fullName;
     private static String bugFullName;
-
+     
     public static void main(String[] args){
         //scanning the strings and storing them in variables
-
+        
         System.out.println("Please enter two String");
         System.out.println();
         System.out.println();
@@ -16,7 +16,7 @@ public class test{
         String firstN = firstName.nextLine();
         Scanner lastName = new Scanner(System.in);
         String lastN = lastName.nextLine();
-        String fullName = firstN + lastN;
+        String fullName = firstN + lastN; 
         System.out.println("The appended string is "+fullName);
 
         //Counting number of character in appended string
@@ -25,51 +25,59 @@ public class test{
             if(fullName.charAt(i) != ' '){
                 fCount++;
             }
-
+            
         }
-        //recreating bug
+        
+        //bug code
         int max = (1000 / fCount);
         int modulo = (1000 % fCount);
         int modulo_string = (int)(Math.random()* modulo);
-        double x = (Math.random() * max);
+        double x = (Math.random() * max); 
         int z = (int) x;
         String bugFullName = fullName.repeat(z);
-
-
-        //num characters of bugged string
+        
+        
+        //num characters of bugstring
         int count = 0;
         for(int i = 0; i< bugFullName.length(); i++){
 
             if(bugFullName.charAt(i) != ' ')
                 count++;
         }
-
+        
         //Scanning specific character to count
-        System.out.println("Please write character to count");
+        System.out.println("Enter a character that needs to be find in the string");
         Scanner character = new Scanner(System.in);
         char fCharacter = character.next().charAt(0);
         String subBug = bugFullName.substring(0, modulo_string);
-        //counting specific characters
+        
+        //counting specific charatres
         int charCount = 0;
         for (int i = 0; i < bugFullName.length(); i++){
-
+        
             if(bugFullName.charAt(i) == fCharacter)
                 charCount++;
-
-
+               
+            
         }
-        //counting specific character in sub string
-        int CharCountMainString = charCount;
+        
+        //counting specific character in substring
+        int charCountMainString = charCount;
         int charCountSub = 0;
         for (int o = 0; o<subBug.length(); o++){
             if(subBug.charAt(o) == fCharacter)
                 charCountSub++;
         }
-        int finalCount = (CharCountMainString + charCountSub);
+        int finalCount = (charCountMainString + charCountSub);
+        
         //printing results
-        System.out.println("Number of characters is  " +count);
+        System.out.println("The number of characters in the string will be  " +count);
         System.out.println(bugFullName + bugFullName.substring(0, modulo_string));
-
-        System.out.println("Character "+fCharacter + " was found in this string for "+finalCount+" times");
+        System.out.println("The number of times the asked character ("+fCharacter+") is executed in this string are : " +finalCount);
+        
+        //BONUS FEATURES
+        
+        
     }
+
 }
